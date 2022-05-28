0fe59fcd31dddfe8cf014bcc908c0e2fc1644d67
Fabio Marangon <fabio_marangon@usmp.pe>
2022-05-28 09:11:56 -0500

    Arreglo merge

--- Models/Pago.cs
+++ Models/Pago.cs
@@ -3,28 +3,6 @@ using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-<<<<<<< HEAD
-namespace Repaso_Net.Models
-{
-    [Table("DataPagos")]
-    public class Pago
-    {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Column("id")]
-        public int Id { get; set; }
-        public DateTime fechaPago { get; set; } = DateTime.Now;
-        public double monto { get; set; }
-        public Usuario usuario { get; set; }
-        public string estado { get; set; }
-        public string metodoPago { get; set; }
-        public string banco { get; set; }
-        public string nombreCuenta { get; set; }
-        public string nombrefile {get; set; }
-         public string fileBase64 {get; set; }
-        public Byte [] archivo {get; set;}
-
-    }
-=======
 
 namespace Repaso_Net.Models {
   
@@ -68,6 +46,4 @@ namespace Repaso_Net.Models {
 
          public string Status { get; set; }
    }
-
->>>>>>> 43dd4014a2a238d8f681cd324fa80b5bfafbda86
 }
