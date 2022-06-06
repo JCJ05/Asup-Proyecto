d58063f93d68e1d34d226a3b6b26c44f9f873468
JCJ05 <julitoaguerorengifo@hotmail.com>
2022-06-06 13:21:31 -0500

    HU-3.3

--- Controllers/InscripcionesController.cs
+++ Controllers/InscripcionesController.cs
@@ -46,6 +46,40 @@ namespace Repaso_Net.Controllers
             return View(inscritos);
         }
 
+        public async Task<IActionResult> listCursosAndCantInscritos(){
+         
+           var cursos = await _context.DataCursos.ToListAsync();
+           int cantidadInscritos = 0;
+           int id = 0;
+            
+            List<Cantidad> cantidades = new List<Cantidad>();
+
+           foreach(var curso in cursos){
+
+                var cantidadModelo = new Cantidad();
+                cantidadModelo.Id = curso.Id;
+                cantidadModelo.Nombre = curso.nombre;
+                cantidadModelo.CantidadInscritos = _context.DataCursoAlumnos.Where(x => x.Curso.Id == curso.Id).Count();
+                 
+                if(cantidadModelo.CantidadInscritos > cantidadInscritos){
+                    cantidadInscritos = cantidadModelo.CantidadInscritos;
+                    id = cantidadModelo.Id;
+                }
+
+                cantidades.Add(cantidadModelo);
+           }
+
+              var cursoMasInscrito = _context.DataCursos.FirstOrDefault(x => x.Id == id);
+              ViewBag.cursoMasInscrito = cursoMasInscrito.nombre;
+              ViewBag.cantidadInscritos = cantidadInscritos;
+
+               List<Cantidad> cantidadesOrdenada = new List<Cantidad>();
+               cantidadesOrdenada = cantidades.OrderByDescending(x => x.CantidadInscritos).ToList();
+        
+           return View(cantidadesOrdenada);
+
+        }
+
         public async Task<ActionResult<String>> findCursosByNombre(string nombre){
            
            var cursos = new List<Curso>();
---
+++ Models/Cantidad.cs
@@ -0,0 +1,13 @@
+
+namespace Repaso_Net.Models 
+
+{
+    public class Cantidad
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int CantidadInscritos { get; set; }
+    }
+
+
+}
